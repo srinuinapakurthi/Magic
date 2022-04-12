7cc4b0173e4c6fe1f7c17ad62a07e0a5a10a53f9
srinuinapakurthi <srinuinapakurthi123@gmail.com>
2022-04-12 11:44:51 +0530

    srinu

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
- 
-@app.route('/greeting/<user>')
-def greeting(user):
-    return render_template('greeting.html',name=user)
-
-if __name__  =='__main__':  
-    app.run(debug = True)
